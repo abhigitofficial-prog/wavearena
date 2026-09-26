56cae2ae3d1988750decd6eadc5bfeb534ddfb53
Ryomen Sukuna <sukuna6873@outlook.com>
2026-09-26 05:47:12 +0000

    added user logout functionality

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js"
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/login").post(verifyJWT, logoutUser);
